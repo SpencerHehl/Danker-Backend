75530e95bb82065d2ca2f1153c396c714f9f2505
SpencerHehl <schehl49@gmail.com>
2020-11-15 18:47:23 -0500

    route implementation

--- src/danks/danks.router.ts
+++ src/danks/danks.router.ts
@@ -1,21 +1,48 @@
 import { Request, Response, Router } from 'express';
+import * as DankController from './danks.controller';
+import { IDank } from './danks.model';
+import { IDankLeaderStat } from './leaderstat.model';
 
 const DanksRouter: Router = Router();
 
 DanksRouter.route('/danke').post((req: Request, res: Response) => {
-    console.log('blah');
+    DankController.submitDank(req)
+        .then((dank: IDank) => {
+            res.status(201).json(dank);
+        })
+        .catch((err) => {
+            res.status(500).json({ error: err });
+        });
 });
 
 DanksRouter.route('/danke/recents').get((req: Request, res: Response) => {
-    console.log('blah');
+    DankController.getRecentDanks()
+        .then((danks: IDank[]) => {
+            res.status(200).json(danks);
+        })
+        .catch((err) => {
+            res.status(500).json({ error: err });
+        });
 });
 
 DanksRouter.route('/leaders/danker').get((req: Request, res: Response) => {
-    console.log('blah');
+    DankController.getDankerLeaders()
+        .then((leaders: IDankLeaderStat[]) => {
+            res.status(200).json(leaders);
+        })
+        .catch((err) => {
+            res.status(500).json({ error: err });
+        });
 });
 
 DanksRouter.route('/leaders/dankee').get((req: Request, res: Response) => {
-    console.log('blah');
+    DankController.getDankeeLeaders()
+        .then((leaders: IDankLeaderStat[]) => {
+            res.status(200).json(leaders);
+        })
+        .catch((err) => {
+            res.status(500).json({ error: err });
+        });
 });
 
 export default DanksRouter;
